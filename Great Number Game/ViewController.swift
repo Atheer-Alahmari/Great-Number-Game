cda42741d1446f4f47e517fd8c992e2ce8eb1b84
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-12-05 16:04:50 +0300

    Great Number Game

--- Great Number Game/ViewController.swift
+++ Great Number Game/ViewController.swift
@@ -9,11 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterNo_TF: UITextField!
+    var randomNumber = Int.random(in: 1...100)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func submet_btn(_ sender: UIButton) {
+        //  let enterNo_TF_No = Int(enterNo_TF.text!)!
 
+        
+        if  let enterNo_Text = enterNo_TF.text{
+         if let enterNo_TF_No = Int(enterNo_Text){
+         
+        if  randomNumber == enterNo_TF_No {
+            //crete Alert
+            let alert = UIAlertController(title: "Correct", message: " \(enterNo_TF_No) Was Correct!!!", preferredStyle: UIAlertController.Style.alert)
+            
+            // add an action button
+            alert.addAction(UIAlertAction(title: "Play Again", style: UIAlertAction.Style.destructive, handler:{UIActionHandler in  self.randomNumber = Int.random(in: 1...100)}))
+    
+            //Change backgroundColor
+            alert.view.subviews.first?.backgroundColor = UIColor.systemGreen
+            
+            // show the alert
+            self.present(alert , animated : true , completion : nil)
+        }
+        //---------------------------------------------------------------
+        
+        else if  randomNumber > enterNo_TF_No {
+            //crete Alert
+            let alert = UIAlertController(title: "Incorrect", message: " \(enterNo_TF_No) is Too Low ", preferredStyle: UIAlertController.Style.alert)
+            // add an action button
+            alert.addAction(UIAlertAction(title: "Guess Again", style: UIAlertAction.Style.default, handler: nil))
+            
+            //Change backgroundColor
+            alert.view.subviews.first?.backgroundColor = UIColor.systemRed
+
+            // show the alert
+            self.present(alert , animated : true , completion : nil)
+        }
+        //----------------------------------------------------------------
+        
+        else {
+            
+            //create Alert
+           let alert = UIAlertController(title: "Incorrect", message: " \(enterNo_TF_No) is Too High ", preferredStyle: UIAlertController.Style.alert)
+            // add an action button
+            alert.addAction(UIAlertAction(title: "Guess Again", style: UIAlertAction.Style.default, handler: nil))
+            //Change backgroundColor
+            alert.view.subviews.first?.backgroundColor = UIColor.systemRed
+
+            // show the alert
+            self.present(alert , animated : true , completion : nil)
+
+        }
+        
+    } //end if  (let enterNo_Text = enterNo_TF.text)
+            
+        } // end if (let enterNo_TF_No = Int(enterNo_Text))
+
+    
+        
+        
+    
+    }
 }
 
